dbb21097f6bbf49f47496ae687de71ca83c0b03e
cara <cara656513@gmail.com>
2024-12-18 15:19:59 +0900

    Image -> img

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -23,7 +23,7 @@ const ItemsPage = async (): Promise<React.JSX.Element> => {
           key={key}
           href={`/items/${key}`}
         >
-          <Image
+          <img
             src={`
               https://ddragon.leagueoflegends.com/cdn/${latestVer}/img/item/${value.image.full}
               `}
